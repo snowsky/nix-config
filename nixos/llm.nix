6519b7880b27bb834c5d7e46daa28bb614e218e0
Hao Wang <hao.1.wang@gmail.com>
2024-10-09 00:05:47 -0400

    add llm file

--- nixos/llm.nix
+++ nixos/llm.nix
@@ -3,9 +3,11 @@
 {
   services.ollama.enable = true;
   services.ollama.loadModels = [ "llama3.2:3b" "dolphin-llama3:8b" "qwen2.5-coder:7b" "llava-llama3:8b" "phi3.5:3.8b" ];
-  services.ollama.acceleration = "cuda";
+  services.ollama.acceleration = "rocm";
   
   environment.systemPackages = with pkgs; [
+    ollama
+    open-webui
     oterm
     alpaca
     nextjs-ollama-llm-ui
